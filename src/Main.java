b6c27840b6b5c2c8691fb588fba22509430127b6
MrOffer <thomas.e.offer@gmail.com>
2019-12-10 09:02:42 -0600

    added the add functionality

--- src/Main.java
+++ src/Main.java
@@ -46,6 +46,18 @@ public class Main {
                 String userInput = scan.nextLine();
                 System.out.println("What is your contacts phone number?");
                 String userInput1 = scan.nextLine();
+                String finalContact = userInput + " | " + userInput1;
+                try {
+                    Path filepath = Paths.get("src", "contacts.txt");
+                    Files.write(
+                            filepath,
+                            Arrays.asList(finalContact), // list with one item
+                            StandardOpenOption.APPEND
+                    );
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                System.out.printf("Your contact: %s was successfully added!",finalContact);
                 break;
             case 3:
                 System.out.println("Type in the contact you are looking for");
